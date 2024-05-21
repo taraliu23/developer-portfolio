af30d9a26de39827cd128a506c1611c9f7f267a5
drunken-boat <rainylty@gmail.com>
2024-05-21 16:36:01 -0400

    Revert "add: info"
    This reverts commit 3437129585e3bce00d110edc162105f019f58c7a.

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,8 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Master of Science in Computational Science and Engineering",
-    duration: "2023 - Present",
-    institution: "Georgia Institute of Technology",
+    title: "Bachelor Degree",
+    duration: "2020 - Present",
+    institution: "National University of Bangladesh",
   },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    duration: "2018 - 2020",
+    institution: "Noakhali Islamia Kamil Madrasah",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2008 - 2018",
+    institution: "Baitus Saif Islamia Madrasah",
+  }
 ]
